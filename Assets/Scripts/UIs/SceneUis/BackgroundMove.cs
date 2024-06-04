17fe2596a9e73c1cb4e07e0f04eaeabd6241b1fd
hance0103 <kmhosu0103@naver.com>
2024-06-04 22:11:04 +0900

    메인화면 뒷 배경 움직이는거 완성, UI 아이콘 교체

--- Assets/Scripts/UIs/SceneUis/BackgroundMove.cs
+++ Assets/Scripts/UIs/SceneUis/BackgroundMove.cs
@@ -4,46 +4,17 @@ using UnityEngine;
 
 public class BackgroundMove : MonoBehaviour
 {
+    private MeshRenderer _render;
+    private float _offset;
     [SerializeField]
-    private GameObject _morningSprite;
-    [SerializeField]
-    private GameObject _eveningSprite;
-
-    [SerializeField]
-    private float speed;
-    [SerializeField]
-    private Transform[] backgrounds;
-
-    private float _leftPosX;
-    private float _rightPosX;
-    private float _xScreenHalfSize;
-    private float _yScreenHalfSize;
-
-    // Start is called before the first frame update
-    void Start()
+    private float _speed;
+    private void Start()
     {
-        _leftPosX = 0f;
-        _rightPosX = 0f;
-        _yScreenHalfSize = Camera.main.orthographicSize;
-        _xScreenHalfSize = _yScreenHalfSize * Camera.main.aspect;
-
-        _leftPosX = -(_xScreenHalfSize * 2);
-        _rightPosX = _xScreenHalfSize * 2 * backgrounds.Length;
+        _render = GetComponent<MeshRenderer>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        for (int i = 0;  i <backgrounds.Length; i++)
-        {
-            backgrounds[i].position += new Vector3(-speed, 0, 0) * Time.deltaTime;
-
-            if (backgrounds[i].position.x < _leftPosX)
-            {
-                Vector3 nextPos = backgrounds[i].position;
-                nextPos = new Vector3(nextPos.x + _rightPosX, nextPos.y, nextPos.z);
-                backgrounds[i].position = nextPos;
-            }
-        }
+        _offset += Time.deltaTime*_speed;
+        _render.material.mainTextureOffset = new Vector2(_offset, 0);
     }
 }
